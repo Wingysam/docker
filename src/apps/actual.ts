85836de45d1a84a6737133eba060d644e0573e83
Wingy <git@wingysam.xyz>
2024-09-21 18:53:52 -0400

    migrate to new format

--- src/apps/actual.ts
+++ src/apps/actual.ts
@@ -3,24 +3,22 @@ import ingress from 'lib/ingress.ts'
 import secrets from 'lib/secrets.ts'
 import App from 'lib/App.ts'
 
-export default async () => {
-  const app: ComposeSpecification = {}
+const app: ComposeSpecification = {}
 
-  app.services = {}
-  app.services.app = {
-    image: 'ghcr.io/actualbudget/actual-server:24.7.0-alpine',
-    environment: {
-      TZ: 'America/New_York',
-      SIGNUPS_ALLOWED: 'false',
-      ADMIN_TOKEN: secrets.actual_admin_token,
-    },
-    volumes: ['/nomad-nfs/actual:/data'],
-  }
-  await ingress(app.services.app, {
-    hostname: 'actual.wing.lol',
-    port: 5006,
-    entrypoint: 'ts-https',
-  })
-
-  return App(app)
+app.services = {}
+app.services.app = {
+  image: 'ghcr.io/actualbudget/actual-server:24.7.0-alpine',
+  environment: {
+    TZ: 'America/New_York',
+    SIGNUPS_ALLOWED: 'false',
+    ADMIN_TOKEN: secrets.actual_admin_token,
+  },
+  volumes: ['/nomad-nfs/actual:/data'],
 }
+await ingress(app.services.app, {
+  hostname: 'actual.wing.lol',
+  port: 5006,
+  entrypoint: 'ts-https',
+})
+
+export const state = App(app)
