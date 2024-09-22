3778105bcf8331e37334cdd117f683ce9d374fdf
Wingy <git@wingysam.xyz>
2024-09-22 10:54:28 -0400

    Remove version from defaults The version property is deprecated and doesn't do anything any more.

--- src/lib/App.ts
+++ src/lib/App.ts
@@ -4,8 +4,6 @@ import type { ComposeSpecification } from 'composepilot/compose-spec'
 export default async function App(
   spec: ComposeSpecification,
 ): Promise<ComposeSpecification> {
-  spec.version = spec.version ?? '3'
-
   if (typeof spec.services !== 'undefined') {
     for (const [_name, service] of Object.entries(spec.services)) {
       service.restart = service.restart ?? 'always'
