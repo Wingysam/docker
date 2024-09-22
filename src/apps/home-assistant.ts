129df59ec2079750229fa693d599d807b935bb2b
Wingy <git@wingysam.xyz>
2024-09-21 21:36:12 -0400

    home-assistant: create config

---
+++ src/apps/home-assistant.ts
@@ -0,0 +1,19 @@
+import type { ComposeSpecification } from 'composepilot/compose-spec.ts'
+import ingress from 'lib/ingress.ts'
+import App from 'lib/App.ts'
+
+const app: ComposeSpecification = {}
+
+app.services = {}
+app.services.app = {
+  image: 'homeassistant/home-assistant:stable',
+  network_mode: 'host',
+  volumes: ['/nomad-nfs/home-assistant/config:/config'],
+}
+await ingress(app.services.app, {
+  hostname: 'home-assistant.wingysam.xyz',
+  entrypoint: 'ts-https',
+  port: 80,
+})
+
+export const state = await App(app)
