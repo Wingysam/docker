ba740c7f6defb4670d5720d8a845c5f55f40a71a
Wingy <git@wingysam.xyz>
2024-09-21 22:52:46 -0400

    home-assistant: revert to host networking

--- src/apps/home-assistant.ts
+++ src/apps/home-assistant.ts
@@ -7,36 +7,13 @@ const app: ComposeSpecification = {}
 app.services = {}
 app.services.app = {
   image: 'homeassistant/home-assistant:stable',
-  networks: {
-    vlan: {
-      ipv4_address: '192.168.1.101',
-    },
-  },
+  network_mode: 'host',
   volumes: ['/nomad-nfs/home-assistant/config:/config'],
 }
 await ingress(app.services.app, {
   hostname: 'home-assistant.wingysam.xyz',
   entrypoint: 'ts-https',
-  port: 80,
+  port: 8123,
 })
-
-app.networks = {
-  vlan: {
-    driver: 'ipvlan',
-    driver_opts: {
-      parent: 'br0',
-      ipvlan_mode: 'l3s',
-    },
-    ipam: {
-      config: [
-        {
-          subnet: '192.168.1.0/24',
-          ip_range: '192.168.1.101/32',
-          gateway: '192.168.1.1',
-        },
-      ],
-    },
-  },
-}
 
 export const state = await App(app)
