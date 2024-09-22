24906bbeb9451a5d221fe2ee82ed07e78c9f1e09
Wingy <git@wingysam.xyz>
2024-09-22 09:31:22 -0400

    traefik: create config

---
+++ src/apps/traefik.ts
@@ -0,0 +1,22 @@
+import type { ComposeSpecification } from 'composepilot/compose-spec.ts'
+import App from 'lib/App.ts'
+import secrets from 'lib/secrets'
+
+const app: ComposeSpecification = {}
+
+app.services = {}
+app.services.app = {
+  image: 'traefik:v2.4.11',
+  network_mode: 'host',
+  environment: {
+    CF_DNS_API_TOKEN: secrets.traefik_cloudflare_token,
+  },
+  volumes: [
+    '/nomad-nfs/traefik/traefik.toml:/etc/traefik/traefik.toml',
+    '/nomad-nfs/traefik/acme:/etc/traefik/acme',
+    '/nomad-nfs/traefik/logs:/logs',
+    '/var/run/docker.sock:/var/run/docker.sock',
+  ],
+}
+
+export const state = await App(app)
