bc76d5bd6ab6dd3af215fafcfe720ba7876e97e1
Wingy <git@wingysam.xyz>
2024-12-09 13:47:42 -0500

    immich: update version

--- src/apps/immich.ts
+++ src/apps/immich.ts
@@ -2,7 +2,7 @@ import type { ComposeSpecification } from 'composepilot/compose-spec.ts'
 import ingress from 'lib/ingress.ts'
 import App from 'lib/App.ts'
 
-const IMMICH_VERSION = 'v1.121.0'
+const IMMICH_VERSION = 'v1.122.0'
 const COMMON_ENV = {
   DB_HOSTNAME: 'database',
   DB_USERNAME: 'postgres',
