71cb91254a3ef36358a2d08d5b5251262d95cbd0
Wingy <git@wingysam.xyz>
2026-06-08 00:00:00 +0000

    Fix tag

--- src/apps/forgejo.ts
+++ src/apps/forgejo.ts
@@ -7,7 +7,7 @@ const app: ComposeSpecification = {}
 app.services = {}
 app.services.forgejo = {
   // renovate: datasource=docker depName=codeberg.org/forgejo/forgejo
-  image: 'codeberg.org/forgejo/forgejo:v15.0.2',
+  image: 'codeberg.org/forgejo/forgejo:15.0.2',
   environment: {
     TZ: 'America/New_York',
     USER_UID: '1000',
